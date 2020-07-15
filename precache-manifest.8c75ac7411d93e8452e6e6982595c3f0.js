b46116b725f7378bac4ba916e1d5a6f53b1a226a
Lisa Wagner <wagnerl@mskcc.org>
2020-07-15 08:44:54 -0400

    Updates

---
+++ precache-manifest.8c75ac7411d93e8452e6e6982595c3f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07dae3aae1ed51573db0f1d79985bd38",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85e1d8927defd7931bf0",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b6945297c11abd38b60e",
+    "url": "/static/js/2.7b7f70aa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7b7f70aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85e1d8927defd7931bf0",
+    "url": "/static/js/main.9abfb727.chunk.js"
+  },
+  {
+    "revision": "63db114f029e5a8a3587",
+    "url": "/static/js/runtime-main.1fd567e1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/lisawagner/precache-manifest.d632159770e948b5d21611b6b05b4708.js"
+  "/precache-manifest.8c75ac7411d93e8452e6e6982595c3f0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/lisawagner/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
